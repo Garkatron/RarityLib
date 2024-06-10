789a153853c38ff3213607d7f0551ff9686298d7
Garkatron <masitacarg@gmail.com>
2024-06-10 16:54:08 +0200

    @Garkatron Not compile(ERROR)

--- src/main/java/deus/rarity_lib/mixin/MixinRarityItem.java
+++ src/main/java/deus/rarity_lib/mixin/MixinRarityItem.java
@@ -1,7 +1,7 @@
 package deus.rarity_lib.mixin;
 
-import deus.rarity_lib.RarityLevel;
 import deus.rarity_lib.Interfaces.IItemRarityMixin;
+import deus.rarity_lib.RarityLevel;
 import net.minecraft.core.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -18,7 +18,6 @@ public class MixinRarityItem implements IItemRarityMixin {
 	@Inject(method = "<init>(I)V", at = @At("TAIL"))
 	private void onConstruct(int id, CallbackInfo ci) {
 		System.out.println("Constructor de Item (int id) invocado con id: " + id);
-
 	}
 
 	@Override
@@ -30,5 +29,15 @@ public class MixinRarityItem implements IItemRarityMixin {
 	public RarityLevel rarityLib$getRarityLevel() {
 		return this.rarityLevel;
 	}
+
+	//@Override
+	//public void rarityLib$setRarityLevel(RarityLevel rarityLevel) {
+		//this.rarityLevel = rarityLevel;
+	//}
+
+	//@Override
+	//public RarityLevel rarityLib$getRarityLevel() {
+		//
+	//}
 
 }
