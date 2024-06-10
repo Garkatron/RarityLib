3ce40948c523dc5e62a794aa8c73ff1cac20a074
Garkatron <145294535+Garkatron@users.noreply.github.com>
2024-06-10 20:37:29 +0200

    1.1.0-7.1_01-babric@Garkatron
    Commit 2
    
    Applied all changes from 7.2pre1 branch
    
    Except for the debug item

---
+++ src/main/java/deus/rarity_lib/Interfaces/mixin/IItemRarityMixin.java
@@ -0,0 +1,11 @@
+package deus.rarity_lib.Interfaces.mixin;
+
+import deus.rarity_lib.RarityLevel;
+
+public interface IItemRarityMixin {
+
+	void rarityLib$setRarityLevel(RarityLevel rarityLevel);
+
+	RarityLevel rarityLib$getRarityLevel();
+
+}
---
+++ src/main/java/deus/rarity_lib/LootTables/LootTable.java
@@ -0,0 +1,60 @@
+package deus.rarity_lib.LootTables;
+
+import net.minecraft.core.WeightedRandomLootObject;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class LootTable {
+
+	private final ThreadLocalRandom rng = ThreadLocalRandom.current();
+	private final Map<Integer, WeightedRandomLootObject> objectsWithProbability;
+
+	public LootTable() {
+		this.objectsWithProbability = new HashMap<>();
+	}
+
+	public LootTable(Map<Integer, WeightedRandomLootObject> items) {
+		this.objectsWithProbability = items;
+	}
+
+	public Item getRandomItem() {
+		if (objectsWithProbability.isEmpty()) {
+			throw new IllegalStateException("No items in the loot table");
+		}
+
+		int randomIndex = rng.nextInt(objectsWithProbability.size());
+		return (Item) objectsWithProbability.values().toArray()[randomIndex];
+	}
+
+
+	public void addItemWithProbability(int probability, Item item, int min_quenty, int max_quenty) {
+		if (probability <= 0 || probability > 100) {
+			throw new IllegalArgumentException("Probability must be between 1 and 100");
+		}
+		int totalProbability = objectsWithProbability.keySet().stream().mapToInt(Integer::intValue).sum() + probability;
+		if (totalProbability > 100) {
+			throw new IllegalArgumentException("Total probability cannot exceed 100%");
+		}
+		objectsWithProbability.put(probability, new WeightedRandomLootObject(item.getDefaultStack(), min_quenty, max_quenty));
+	}
+
+	public ItemStack getRandomItemWithProbability() {
+		if (objectsWithProbability.isEmpty()) {
+			throw new IllegalStateException("No items in the loot table");
+		}
+		int n = rng.nextInt(1, 101);
+		int cumulativeProbability = 0;
+		for (int probability : objectsWithProbability.keySet()) {
+			cumulativeProbability += probability;
+			if (n <= cumulativeProbability) {
+				ItemStack stack = objectsWithProbability.get(probability).getItemStack();
+				return new ItemStack(stack.itemID, 1, stack.getMetadata(), stack.getData());
+			}
+		}
+		return null;
+	}
+}
